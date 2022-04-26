4716882852926c0db05d5b6bf3bcbd9abb21d21f
Matt Schwarz <mattschwarz5@gmail.com>
2022-04-26 14:33:12 -0500

    Fixed bug preventing deployment

--- src/database.js
+++ src/database.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
